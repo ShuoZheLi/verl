64836ef088b78645426a1b8400e3667387caf30c
ShuoZheLi <lishuozhe1999@163.com>
2026-05-27 17:56:27 -0500

    modified:   value_decoding/script/train_search_induced_critic_multinode.sh

--- value_decoding/script/train_search_induced_critic_multinode.sh
+++ value_decoding/script/train_search_induced_critic_multinode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_simc_critic
 #SBATCH --account=ECS26006
-#SBATCH --partition=gh-dev
+#SBATCH --partition=gh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=72
