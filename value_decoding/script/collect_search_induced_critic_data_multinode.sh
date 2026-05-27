93e22d4384a9fceba7b2558c12d9649bc2cff5a7
ShuoZheLi <lishuozhe1999@163.com>
2026-05-26 21:15:57 -0500

    modified:   value_decoding/script/collect_search_induced_critic_data.sh 	modified:   value_decoding/script/collect_search_induced_critic_data_multinode.sh 	modified:   value_decoding/script/collect_search_induced_critic_data_multinode_test.sh

--- value_decoding/script/collect_search_induced_critic_data_multinode.sh
+++ value_decoding/script/collect_search_induced_critic_data_multinode.sh
@@ -123,7 +123,12 @@ SCRIPT_PATH="${WORK_DIR}/value_decoding/script/$(basename "${BASH_SOURCE[0]}")"
 sync_to_work() {
   echo "Syncing run directory back to WORK..."
   mkdir -p "$ARCHIVE_DIR"
-  rsync -a "$RUN_DIR"/ "$ARCHIVE_DIR"/ || true
+  rsync -a \
+    --exclude='merged_hf/' \
+    --exclude='merged_hf/***' \
+    --exclude='shards/*/merged_hf/' \
+    --exclude='shards/*/merged_hf/***' \
+    "$RUN_DIR"/ "$ARCHIVE_DIR"/ || true
   echo "Archived run to: $ARCHIVE_DIR"
 }
 
